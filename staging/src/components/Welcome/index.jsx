5f7a8d27081d58baa31b8f8882bfdbdf41de1be4
熊雷 <xionglei@jiuqi.com.cn>
2022-07-11 18:01:59 +0800

    todo 055

--- staging/src/components/Welcome/index.jsx
+++ staging/src/components/Welcome/index.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import welcome from './index.module.css'
 
 export default class Index extends Component {
+    
     render() {
         return (
             <h2 className={welcome.title}>Welcome</h2>
