94878d7eae3677b29d8e5c13b2b82fd9cdda44c1
熊雷 <xionglei@jiuqi.com.cn>
2022-07-11 17:36:54 +0800

    组件引入的简写

---
+++ staging/src/components/Hello/index.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './index.css'
+
+export default class Index extends Component {
+
+    render() {
+        return (
+            <h2 className='title'>Hello, World!</h2>
+        )
+    }
+}
