250f1185729e83d890354e3166a168fdd5ed3884
熊雷 <xionglei@jiuqi.com.cn>
2022-07-14 14:37:33 +0800

    添加鼠标移入移出事件

--- staging/src/components/Item/index.jsx
+++ staging/src/components/Item/index.jsx
@@ -5,35 +5,54 @@ import './index.css';
 class Item extends Component {
 
     static propTypes = {
-		item:PropTypes.object.isRequired,
-		modifyItemState:PropTypes.func.isRequired,
-		delItem:PropTypes.func.isRequired,
-	}
+        item: PropTypes.object.isRequired,
+        modifyItemState: PropTypes.func.isRequired,
+        delItem: PropTypes.func.isRequired,
+    }
+
+    state = {
+        // 处理鼠标移入显示
+        mouseFlag: false,
+    }
 
     handleClick = (item) => {
         const {delItem} = this.props;
-        return (e) => {
+        return () => {
             delItem(item.id);
         }
     }
 
     handleCheck = (item) => {
         const {modifyItemState} = this.props;
-        return (e) => {
+        return () => {
             modifyItemState(item.id, !item.done);
+        }
+    }
+
+    handleMouse = (mouseFlag) => {
+        return () => {
+            this.setState({mouseFlag})
         }
     }
 
     render() {
         const {item} = this.props;
+        const {mouseFlag} = this.state;
         return (
-            <li>
+            <li onMouseEnter={this.handleMouse(true)}
+                onMouseLeave={this.handleMouse(false)}
+                style={{backgroundColor: mouseFlag ? '#add' : '#EEE'}}
+            >
                 <label htmlFor="">
                     <input type="checkbox" checked={item.done} onChange={this.handleCheck(item)}/>
                     <span>{item.name}</span>
                 </label>
-                <button className='btn btn-danger' onClick={this.handleClick(item)}>
-                删除
+                <button
+                    className='btn btn-danger'
+                    onClick={this.handleClick(item)}
+                    style={{display: mouseFlag ? 'block' : 'none'}}
+                >
+                    删除
                 </button>
             </li>
         );
