996c0733f05911ef199625ca3b5abfe9f4100651
lei <xionglei1614@163.com>
2022-07-26 21:40:56 +0800

    NavLink标签比Link标签多个activeClassName属性, 可给选中的加上该样式。不写，默认加active

--- staging/src/App.jsx
+++ staging/src/App.jsx
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
-import {Link, BrowserRouter, Route} from 'react-router-dom';
-import About from './components/About';
-import Home from './components/Home';
+import {NavLink, Route} from 'react-router-dom';
+// 路由组件
+import About from './pages/About';
+import Home from './pages/Home';
+// 一般组件
+import Header from './components/Header'
+import './App.css'
 
 class App extends Component {
 
@@ -9,11 +13,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <div className='row'>
-                    <div className='col-xs-offset-2 col-xs-8'>
-                        <div className="page-header"><h2>React Router Demo</h2></div>
-                    </div>
-                </div>
+                <Header/>
                 <div className="row">
                     <div className="col-xs-2 col-xs-offset-2">
                         <div className="list-group">
@@ -23,8 +23,8 @@ class App extends Component {
 							<a className="list-group-item active" href="./home.html">Home</a> */}
 
                             {/* 在React中靠路由链接实现切换组件--编写路由链接 */}
-                            <Link className="list-group-item" to="/about">About</Link>
-                            <Link className="list-group-item" to="/home">Home</Link>
+                            <NavLink activeClassName='xl' className="list-group-item" to="/about">About</NavLink>
+                            <NavLink activeClassName='xl' className="list-group-item" to="/home">Home</NavLink>
                         </div>
                     </div>
                     <div className="col-xs-6">
--- staging/src/pages/Home/index.jsx
+++ staging/src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 class Home extends Component {
     render() {
+        // console.log("Home组件收到的props", this.props)
         return (
             <div>
                 Home
