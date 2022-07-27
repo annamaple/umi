4bb72616986a0cee624c49bdc217d65b086bc93f
lei <xionglei1614@163.com>
2022-07-27 23:50:45 +0800

    编程式路由导航-about定制跳转message

--- staging/src/pages/Home/News/index.jsx
+++ staging/src/pages/Home/News/index.jsx
@@ -1,6 +1,18 @@
 import React, {Component} from 'react';
 
 class Index extends Component {
+
+    componentDidMount() {
+        this.timer = setTimeout(() => {
+            this.props.history.push('/home/message')
+        }, 3000)
+    }
+
+    componentWillUnmount() {
+        console.log('timer close');
+        clearInterval(this.timer);
+    }
+
     render() {
         const nums = [1, 2, 3];
         return (
