5232540f33a699f8aee1468c3a04d760267b2563
熊雷 <xionglei@jiuqi.com.cn>
2022-07-27 11:50:07 +0800

    归档-封装(encapsulate)NavLink

---
+++ staging/10_src_encapsulate_NavLink/App.jsx
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
+// 路由组件
+import About from './pages/About';
+import Home from './pages/Home';
+// 一般组件
+import Header from './components/Header';
+import MyNavLink from './components/MyNavLink';
+import './App.css'
+
+class App extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                <div className="row">
+                    <div className="col-xs-2 col-xs-offset-2">
+                        <div className="list-group">
+
+                            {/* 原生html中，靠<a>跳转不同的页面 */}
+                            {/* <a className="list-group-item" href="./about.html">About</a>
+							<a className="list-group-item active" href="./home.html">Home</a> */}
+
+                            {/* 在React中靠路由链接实现切换组件--编写路由链接 */}
+                            {/*<NavLink activeClassName='xl' className="list-group-item" to="/about">About</NavLink>*/}
+                            {/*<NavLink activeClassName='xl' className="list-group-item" to="/home">Home</NavLink>*/}
+                            <MyNavLink to='/home'>Home</MyNavLink>
+                            <MyNavLink to='/about'>About</MyNavLink>
+                        </div>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="panel">
+                            <div className="panel-body">
+                                {/* 注册路由 */}
+                                <Route path="/home" component={Home}/>
+                                <Route path="/about" component={About}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ staging/10_src_encapsulate_NavLink/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    render() {
+        // console.log("Home组件收到的props", this.props)
+        return (
+            <div>
+                Home
+            </div>
+        );
+    }
+}
+
+export default Home;
