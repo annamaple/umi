8027b347f5e9c7e86d61fd421ba5068165f4cedf
熊雷 <xionglei@jiuqi.com.cn>
2022-07-29 15:24:03 +0800

    调整标题名

--- project/annamaple/src/App.jsx
+++ project/annamaple/src/App.jsx
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Header from './components/Head';
+import Body from './components/Body';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header>AnnaMaple</Header>
+                <Body/>
+            </div>
+        );
+    }
 }
 
 export default App;
